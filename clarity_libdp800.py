e0784dd5139f4ed34d8ecd22808f41f5fc3bea60
Wei-Hsun Lin <wxlin1983@users.noreply.github.com>
2017-03-10 12:21:28 -0800

    Update clarity_libdp800.py

--- clarity_libdp800.py
+++ clarity_libdp800.py
@@ -10,40 +10,52 @@ class dp800():
         self.ser = []
 
     def connect(self):
+
         if not self.connecting:
             self.ser = serial.Serial(self.port, baudrate=9600, timeout=3)
             if self.ser.is_open:
                 self.connecting = True
 
     def disconnect(self):
+
         if self.connecting:
             self.ser.close()
             self.connecting = False
 
     def write(self, message):
+
         if not self.connecting:
             self.connect()
         if self.connecting:
             self.ser.write(str.encode(message) + b'\r\n')
-            time.sleep(0.032)
+            time.sleep(0.064)
 
-    def output(self, ch, onoff):
+    def read(self, second=0):
+
+        if not self.connecting:
+            raise Exception('Communication failed.')
+        if self.connecting:
+            time.sleep(second)
+            return self.ser.read(self.ser.in_waiting)
+
+    def getch(self, ch):
 
         if not ((ch == 1) or (ch == 2) or (ch == 3)):
             raise ValueError('Please select CH 1~3')
 
-        if onoff:
-            self.write(':OUTP CH' + str(ch) + ',ON')
-        else:
-            self.write(':OUTP CH' + str(ch) + ',OFF')
+        return 'CH' + str(ch)
 
     def ch(self, mych):
 
-        if not ((mych == 1) or (mych == 2) or (mych == 3)):
-            raise ValueError('Please select CH 1~3')
-
-        command = ':INST CH' + str(mych)
+        command = ':INST ' + self.getch(mych)
         self.write(command)
+
+    def output(self, ch, onoff):
+
+        if onoff:
+            self.write(':OUTP ' + self.getch(ch) + ',ON')
+        else:
+            self.write(':OUTP ' + self.getch(ch) + ',OFF')
 
     def v(self, mych, voltage):
 
@@ -51,7 +63,7 @@ class dp800():
         command = ':VOLT ' + (str(float(voltage)) + '0000')[:4]
         self.write(command)
 
-    def v_protect(self, mych, voltage):
+    def protect_v(self, mych, voltage):
 
         self.ch(mych)
         command = ':VOLT:PROT ' + (str(float(voltage)) + '0000')[:4]
@@ -59,7 +71,7 @@ class dp800():
         command = ':VOLT:PROT:STAT ON'
         self.write(command)
 
-    def v_protect_off(self, mych):
+    def protect_v_off(self, mych):
 
         self.ch(mych)
         command = ':VOLT:PROT:STAT OFF'
@@ -71,7 +83,7 @@ class dp800():
         command = ':CURR ' + (str(float(current)) + '0000')[:4]
         self.write(command)
 
-    def i_protect(self, mych, current):
+    def protect_i(self, mych, current):
 
         self.ch(mych)
         command = ':CURR:PROT ' + (str(float(current)) + '0000')[:4]
@@ -79,8 +91,14 @@ class dp800():
         command = ':CURR:PROT:STAT ON'
         self.write(command)
 
-    def i_protect_off(self, mych):
+    def protect_i_off(self, mych):
 
         self.ch(mych)
         command = ':CURR:PROT:STAT OFF'
         self.write(command)
+
+    def read_i(self, mych):
+
+        command = ':MEAS:CURR? ' + self.getch(mych)
+        self.write(command)
+        return self.read(0)
